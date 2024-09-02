51dfec4d30662e697abed324b60d4ab65985ccbb
Alexis Paz <alexismpaz@gmail.com>
2024-09-02 15:12:25 -0300

    Fix: Category Entity

--- backend/src/categories/categories.module.ts
+++ backend/src/categories/categories.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { categoryProvider } from './provider/categories.provider';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, ...categoryProvider],
 })
 export class CategoriesModule {}
