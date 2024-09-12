58986eb13cb4583e3a7d2ee7b60955158cd02922
Thiago <thiagosalaberry99@gmail.com>
2024-09-12 11:48:14 -0300

    FEAT: Code Plus Page mobile&desktop; PricingCard mobile&desktop

--- frontend/components/index.tsx
+++ frontend/components/index.tsx
@@ -4,10 +4,12 @@ import { Features } from "./Features";
 import { Footer } from "./Footer";
 import { Hero } from "./Hero";
 import { Home } from "./Home";
+import { Loading } from "./loading";
 import { Login } from "./Login";
 import { Navbar} from "./Navbar";
+import { PricingCard } from "./pricing-card";
 import { Register } from "./Register";
 import { SignupForm } from "./signup-form";
 import { UseDetails } from "./Use-Details";
 
-export { AboutUs, DownloadApp, Features, Footer, Hero, Home, Login, Navbar, Register, SignupForm, UseDetails };
+export { AboutUs, DownloadApp, Features, Footer, Hero, Home, Loading, Login, Navbar, PricingCard, Register, SignupForm, UseDetails };
---
+++ frontend/components/pricing-card/index.tsx
@@ -0,0 +1,32 @@
+"use client"
+import styles from "./styles.module.css";
+import { PricingCardProps } from "@/lib/types";
+import { Button } from "@/ui-atoms";
+import { Check } from "lucide-react";
+
+export function PricingCard(props:PricingCardProps) {
+    return (
+        <div className={`${styles.container} ${props.recommended && styles.recommended}`}>
+            {props.recommended && <p className={styles.recommended_p}>Recomendado</p>}
+            <div className={styles.title_container}>
+                <h3 className={styles.title}>{props.title}</h3>
+                <p className={styles.desc}>{props.description}</p>
+            </div>
+            <div className={styles.price_container}>
+                <h3 className={styles.price}>{props.price}</h3>
+                <p className={styles.limit}>{props.limit}</p>
+            </div>
+            <ul className={styles.benefit_list}>
+                {props.benefits.map((b, index) => {
+                    return (
+                        <li key={b} className={styles.benefit_item}>
+                            <Check color="var(--color-primary)" size={20}/><p>{b}</p>
+                        </li>
+                )
+                })}
+            </ul>
+            <Button onClick={props.onClick} variant={props.button.variant}>{props.button.text}</Button>
+        </div>
+    );
+};
+                        
